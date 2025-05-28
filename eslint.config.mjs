c12f04813b6cacdcb91818bef9d43bbd56542aa7
Heeju Park <zero950@gmail.com>
2025-05-28 21:23:08 +0900

    refactor: eslint 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,32 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import prettierPlugin from 'eslint-plugin-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs']
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node
-      },
-      parser: tseslint.parser,
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname
-      }
+export default tseslint.config({
+  ignores: ['eslint.config.mjs'],
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, ...tseslint.configs.recommended],
+  files: ['src/**/*.{ts,js}'],
+  languageOptions: {
+    globals: {
+      ...globals.node
+    },
+    parser: tseslint.parser,
+    sourceType: 'commonjs',
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname
     }
   },
-  {
-    rules: {
-      'prettier/prettier': 'warn',
-      '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    }
+  plugins: {
+    prettier: prettierPlugin
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn'
   }
-);
+});
