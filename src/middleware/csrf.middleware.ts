739fdc6a4e87dc2a50990483f852eb2caa250bb1
hjpark625 <zero950@gmail.com>
2025-06-11 22:29:14 +0900

    feat: csrf 미들웨어 생성

---
+++ src/middleware/csrf.middleware.ts
@@ -0,0 +1,21 @@
+import { doubleCsrf } from 'csrf-csrf';
+
+const csrfMiddleware = doubleCsrf({
+  getSecret: () => process.env.CSRF_SECRET_KEY || 'default',
+  getSessionIdentifier: (req) => {
+    return typeof req.cookies['connect.sid'] === 'string' ? req.cookies['connect.sid'] : '';
+  },
+  getCsrfTokenFromRequest: (req) => {
+    console.log(req.headers);
+    return typeof req.headers['x-csrf-token'] === 'string' ? req.headers['x-csrf-token'] : '';
+  },
+  cookieName: 'x-csrf-token',
+  cookieOptions: { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'strict' },
+  errorConfig: {
+    statusCode: 403,
+    message: '유효하지않은 CSRF 토큰입니다.'
+  },
+  skipCsrfProtection: (req) => req.path.startsWith('/health')
+});
+
+export { csrfMiddleware };
