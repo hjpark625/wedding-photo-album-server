5e809e9d42e436e9f37163e5382e5a96b49916ff
hjpark625 <zero950@gmail.com>
2025-06-11 22:35:13 +0900

    refactor: csrf 토큰 유효시간 설정

--- src/middleware/csrf.middleware.ts
+++ src/middleware/csrf.middleware.ts
@@ -10,7 +10,12 @@ const csrfMiddleware = doubleCsrf({
     return typeof req.headers['x-csrf-token'] === 'string' ? req.headers['x-csrf-token'] : '';
   },
   cookieName: 'x-csrf-token',
-  cookieOptions: { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'strict' },
+  cookieOptions: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 1000 * 60 * 60 * 12 // 12 hours
+  },
   errorConfig: {
     statusCode: 403,
     message: '유효하지않은 CSRF 토큰입니다.'
