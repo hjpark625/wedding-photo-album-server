048d128a895111dcdb53c7d6022be37ccd40ae4b
hjpark625 <zero950@gmail.com>
2025-06-11 23:14:36 +0900

    refactor: cookie samesite 속성 및 도메인 추가
    - 프론트, 백 도메인 차이 문제

--- src/middleware/csrf.middleware.ts
+++ src/middleware/csrf.middleware.ts
@@ -1,26 +1,32 @@
 import { doubleCsrf } from 'csrf-csrf';
 
-const csrfMiddleware = doubleCsrf({
-  getSecret: () => process.env.CSRF_SECRET_KEY || 'default',
-  getSessionIdentifier: (req) => {
-    return typeof req.cookies['connect.sid'] === 'string' ? req.cookies['connect.sid'] : '';
-  },
-  getCsrfTokenFromRequest: (req) => {
-    console.log(req.headers);
-    return typeof req.headers['x-csrf-token'] === 'string' ? req.headers['x-csrf-token'] : '';
-  },
-  cookieName: 'x-csrf-token',
-  cookieOptions: {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge: 1000 * 60 * 60 * 12 // 12 hours
-  },
-  errorConfig: {
-    statusCode: 403,
-    message: '유효하지않은 CSRF 토큰입니다.'
-  },
-  skipCsrfProtection: (req) => req.path.startsWith('/health')
-});
+import type { DoubleCsrfConfigOptions } from 'csrf-csrf';
+
+function csrfMiddleware({ domain, nodeEnv, secretKey }: { secretKey: string; domain: string; nodeEnv: string }) {
+  const csrfConfig: DoubleCsrfConfigOptions = {
+    getSecret: () => secretKey || 'default',
+    getSessionIdentifier: (req) => {
+      return typeof req.cookies['connect.sid'] === 'string' ? req.cookies['connect.sid'] : '';
+    },
+    getCsrfTokenFromRequest: (req) => {
+      return typeof req.headers['x-csrf-token'] === 'string' ? req.headers['x-csrf-token'] : '';
+    },
+    cookieName: 'x-csrf-token',
+    cookieOptions: {
+      httpOnly: true,
+      secure: nodeEnv === 'production',
+      sameSite: nodeEnv === 'production' ? 'none' : 'strict',
+      domain: nodeEnv === 'production' ? domain : undefined,
+      maxAge: 1000 * 60 * 60 * 12
+    },
+    errorConfig: {
+      statusCode: 403,
+      message: '유효하지않은 CSRF 토큰입니다.'
+    },
+    skipCsrfProtection: (req) => req.path.startsWith('/health')
+  };
+
+  return doubleCsrf(csrfConfig);
+}
 
 export { csrfMiddleware };
