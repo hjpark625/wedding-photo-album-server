5f6155c7d380df20b34c600587031f6285c7ec30
hjpark625 <zero950@gmail.com>
2025-06-11 23:14:57 +0900

    refactor: csrf middleware에 필요한 환경변수값 추가 전달

--- src/csrf/csrf.service.ts
+++ src/csrf/csrf.service.ts
@@ -1,12 +1,19 @@
 import { csrfMiddleware } from '@/middleware/csrf.middleware';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import type { Request, Response } from 'express';
 
 @Injectable()
 export class CsrfService {
+  constructor(private configService: ConfigService) {}
+
   onIssueCsrfToken(req: Request, res: Response) {
-    const token = csrfMiddleware.generateCsrfToken(req, res);
+    const nodeEnv = this.configService.get<string>('NODE_ENV') || 'development';
+    const domain = this.configService.get<string>('MAIN_DOMAIN') || 'localhost';
+    const secretKey = this.configService.get<string>('CSRF_SECRET_KEY') || 'default';
+
+    const token = csrfMiddleware({ nodeEnv, domain, secretKey }).generateCsrfToken(req, res);
     return res.json({ csrfToken: token });
   }
 }
