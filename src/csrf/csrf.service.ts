e629c8c02bb2b8884cf70ee611fecd910fccde46
hjpark625 <zero950@gmail.com>
2025-06-11 23:30:57 +0900

    refactor: csrf 미들웨어 인스턴스 생성자 내에서 활용하는 방식으로 변경

--- src/csrf/csrf.service.ts
+++ src/csrf/csrf.service.ts
@@ -1,19 +1,24 @@
 import { csrfMiddleware } from '@/middleware/csrf.middleware';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import type { DoubleCsrfUtilities } from 'csrf-csrf';
 
 import type { Request, Response } from 'express';
 
 @Injectable()
 export class CsrfService {
-  constructor(private configService: ConfigService) {}
+  private csrfMiddlewareInstance: DoubleCsrfUtilities;
 
-  onIssueCsrfToken(req: Request, res: Response) {
+  constructor(private configService: ConfigService) {
     const nodeEnv = this.configService.get<string>('NODE_ENV') || 'development';
     const domain = this.configService.get<string>('MAIN_DOMAIN') || 'localhost';
     const secretKey = this.configService.get<string>('CSRF_SECRET_KEY') || 'default';
 
-    const token = csrfMiddleware({ nodeEnv, domain, secretKey }).generateCsrfToken(req, res);
+    this.csrfMiddlewareInstance = csrfMiddleware({ nodeEnv, domain, secretKey });
+  }
+
+  onIssueCsrfToken(req: Request, res: Response) {
+    const token = this.csrfMiddlewareInstance.generateCsrfToken(req, res);
     return res.json({ csrfToken: token });
   }
 }
