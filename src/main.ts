18731de48a63deade2d0ebd4b09300117312f674
hjpark625 <zero950@gmail.com>
2025-06-11 23:15:13 +0900

    refactor: csrf 미들웨어 환경변수값 전달

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,19 @@ import { AppModule } from '@/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
-  console.info(`NestJS application is starting... in port: ${PORT}`);
+  const nodeEnv = process.env.NODE_ENV || 'development';
+  const domain = process.env.MAIN_DOMAIN || 'localhost';
+  const secretKey = process.env.CSRF_SECRET_KEY || 'default';
 
   app.use(cookieParser());
-  app.use(csrfMiddleware.doubleCsrfProtection);
+  app.use(csrfMiddleware({ nodeEnv, domain, secretKey }).doubleCsrfProtection);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: [process.env.CORS_ORIGIN, 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
   });
 
+  console.info(`NestJS application is starting... in port: ${PORT}`);
   await app.listen(PORT);
 }
 
