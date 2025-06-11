9bc8e7b3c91e834a122fdc1336a90e2e4e2698ca
hjpark625 <zero950@gmail.com>
2025-06-11 22:35:31 +0900

    refactor: csrf 미들웨어 부트스트랩 적용

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+
+import { csrfMiddleware } from '@/middleware/csrf.middleware';
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
   console.info(`NestJS application is starting... in port: ${PORT}`);
+
+  app.use(cookieParser());
+  app.use(csrfMiddleware.doubleCsrfProtection);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+  });
+
   await app.listen(PORT);
 }
 
